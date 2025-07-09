c7db7eae5fd4c393b133705348cab4004bcc2104
Delray-J <delrayjuel@gmail.com>
2025-07-09 23:25:44 +0200

    changed stylng

--- src/index.js
+++ src/index.js
@@ -13,14 +13,16 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "10aa4a04dbbt55363b01c10f31ba8fof";
   let context =
-    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br /> Seperate the french translation and english translation with a <br/> with basic HTML dont add the word html but add the words English Translation. Add an english translation too underline the wordS English Translation in HTML. Make sure to follow the user instructions. Do not include a title to the poem. ";
   let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value} ⌛</div>`;
+
   axios.get(apiURL).then(displayPoem);
 }
  
-
  let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
